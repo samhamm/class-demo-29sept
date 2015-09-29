10c73ee2188ad9b1b5a2977bed8544470afc4bd7
samhamm <samhamm@gmail.com>
2015-09-29 15:17:14 -0700

    here is a commit

---
+++ demo.js
@@ -0,0 +1,11 @@
+var question1 = prompt("Was it easy to get to CF today?");
+var question2 = prompt('Does this room have nice windows?');
+var question3 = prompt('Am I hungry?');
+
+console.log('Question 1 response: ' + question1);
+console.log('Question 2 response: ' + question2);
+console.log('Question 3 response: ' + question3);
+
+alert('Your answer to Question 1 was ' + question1);
+alert('Your answer to Question 2 was ' + question2);
+alert('Your answer to Question 3 was' + question3);
